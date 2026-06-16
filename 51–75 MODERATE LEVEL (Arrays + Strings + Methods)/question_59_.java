47bb152c424e69755da648d88b96deef303dc3ce
Omkawalkar <95124394+Omkawalkar@users.noreply.github.com>
2026-06-16 10:38:58 +0530

    Add files via upload

---
+++ 51–75 MODERATE LEVEL (Arrays + Strings + Methods)/question_59_.java
@@ -0,0 +1,27 @@
+
+public class question_59_ {
+    public static void main(String[] args) {
+        int array[] = { 2, 20, 3, 30, 4, 40, 5, 50 };
+
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+
+                if (array[i] > array[j]) {
+
+                    int temp = array[i];
+
+                    array[i] = array[j];
+
+                    array[j] = temp;
+
+                }
+            }
+
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+}
