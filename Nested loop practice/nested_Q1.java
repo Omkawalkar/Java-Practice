3f256dc63e4722866a623f852a77a2cc20ddd2fa
Omkawalkar <95124394+Omkawalkar@users.noreply.github.com>
2026-06-29 20:33:04 +0530

    Add files via upload

---
+++ Nested loop practice/nested_Q1.java
@@ -0,0 +1,10 @@
+//Print numbers using one loop
+public class nested_Q1 {
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
