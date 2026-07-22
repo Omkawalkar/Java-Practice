e54ea67b3f65e1f81e8db877bb6d6a055dd9fa7b
nemo <omkawalkar86@gmail.com>
2026-07-23 02:44:53 +0530

    new problem

---
+++ Sorting/sorting_4th.java
@@ -0,0 +1,32 @@
+public class sorting_4th {
+
+    public static void main(String[] args) {
+
+        // Bubble Sort in Ascending Order
+
+        int array[] = {5, 3, 8, 1, 2};
+
+        for (int i = 0; i < array.length - 1; i++) {
+
+            for (int j = 0; j < array.length - i - 1; j++) {
+
+                if (array[j] > array[j + 1]) {
+
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+
+                }
+
+            }
+
+        }
+
+        // Print Sorted Array
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+
+    }
+
+}
