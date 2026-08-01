7315f1f4f20e34cc4968351c974fdbc6452fe3cd
nemo <omkawalkar86@gmail.com>
2026-08-02 02:37:59 +0530

    recursion15

---
+++ Recursion/recursion15.java
@@ -0,0 +1,17 @@
+public class recursion15 {
+
+
+    public static int countDigit(int n){
+
+        if(n==0){
+            return 0;
+        }
+
+        return 1+countDigit(n/10);
+    }
+    public static void main(String[] args) {
+        
+int n = 12345;
+System.out.println(countDigit(n));
+    }
+}
--- Recursion/recurssion14.java
+++ Recursion/recurssion14.java
@@ -1,25 +1,20 @@
 public class recurssion14 {
 
-public static int power(int x , int n){
-    if(n==0){
-        return 1;
+    public static int power(int x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+
+        return x * power(x, n - 1);
+
     }
 
-    return x *power(x, n-1);
-
-
-
-}
-
-
-
     public static void main(String[] args) {
-        
-int x = 2;
-int n = 5;
 
-System.out.println(power(x, n));
+        int x = 2;
+        int n = 5;
 
+        System.out.println(power(x, n));
 
     }
 }
