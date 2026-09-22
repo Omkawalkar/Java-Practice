c7430683fb2057ea79194aa8277ddfcdcb15faf6
Nemo <omkawalkar86@gmail.com>
2026-09-22 21:55:50 +0530

    update

--- Recursion/recursion18.java
+++ Recursion/recursion18.java
@@ -1,29 +1,46 @@
 public class recursion18 {
     
+// int function
+//     public static int factorial(int n ){
+//         if( n == 0 || n == 1){
+//             return 1 ;
+//         }
 
-    public static int factorial(int n ){
-        if( n == 0 || n == 1){
-            return 1 ;
-        }
+// int fact = factorial(n - 1 );
 
-int fact = factorial(n - 1 );
+// int cal = n * fact;
 
-int cal = n * fact;
+// return cal ;
 
-return cal ;
+//     }
 
+
+//  void function
+
+public static void recursion(int n,int fact){
+
+    if( n == 0 | n == 1){
+        System.out.println(" factorial is "+ fact);
+return ;
     }
 
+    fact = fact * n;
+   recursion(n - 1, fact);
 
+
+
+
+
+}
 
 
     public static void main(String[] args) {
         
 
         int n = 5 ;
-
-        int fact = factorial(n);
-        System.out.println(fact);
+recursion(n, 1);
+        // int fact = factorial(n);
+        // System.out.println(fact);
 
     }
 }
