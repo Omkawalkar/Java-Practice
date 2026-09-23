b2958abc7a325714bbeccb7d32bbe3c7bf76eb46
Nemo <omkawalkar86@gmail.com>
2026-09-24 02:43:41 +0530

    max digit

--- Recursion/recursion25.java
+++ Recursion/recursion25.java
@@ -2,7 +2,7 @@
 import java.util.Scanner;
 
 public class recursion25 {
-
+// reverse number function 
     public static int reverse(int n, int rev) {
         if (n == 0) {
             return rev;
@@ -12,19 +12,19 @@ public class recursion25 {
         rev = rev * 10 + digit;
         return reverse(n / 10, rev);
     }
+// check palndrome or not 
+    public static void palindrome(int reverse, int n) {
 
-    public static boolean palindrome(int reverse, int n) {
-
-        return n == reverse;
-        // if (n == reverse) {
-        //     System.out.println("given number " + n + "is palindrome");
-        // } else {
-        //     System.out.println("given number " + n + " is not palindrome");
-        // }
+        if (n == reverse) {
+            System.out.println("given number " + n + "is palindrome");
+        } else {
+            System.out.println("given number " + n + " is not palindrome");
+        }
 
     }
 
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println(" Check Number Is palindrome Or Not");
         System.out.println("Enter number");
