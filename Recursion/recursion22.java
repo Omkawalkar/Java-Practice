5083dfa3c4490f66cf89c6cd81bc320652210d74
Nemo <omkawalkar86@gmail.com>
2026-09-23 13:23:30 +0530

    recursion22

--- Recursion/recursion22.java
+++ Recursion/recursion22.java
@@ -3,10 +3,11 @@ public class recursion22 {
 
 
 public static int countDigit(int n){
-    if(n==0){
-        return 0;
-    }
-    return 1 + countDigit( n/10);
+ if ( n == 0){
+    return 0;
+ }
+
+ return 1 + countDigit( n/10 );
 }
 
     public static void main(String[] args) {
