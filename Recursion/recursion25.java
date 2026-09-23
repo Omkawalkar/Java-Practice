0ad130a15df67b6156777b006a66f111629e7b0e
Nemo <omkawalkar86@gmail.com>
2026-09-24 02:20:46 +0530

    m

--- Recursion/recursion25.java
+++ Recursion/recursion25.java
@@ -1,44 +1,39 @@
 
 import java.util.Scanner;
+
 public class recursion25 {
 
-public static int reverse(int n , int rev){
-    if( n == 0){
-        return rev;
+    public static int reverse(int n, int rev) {
+        if (n == 0) {
+            return rev;
+        }
+
+        int digit = n % 10;
+        rev = rev * 10 + digit;
+        return reverse(n / 10, rev);
     }
 
-    int digit = n %10;
-    rev = rev * 10 + digit;
-    return reverse(n /10, rev);
-}
+    public static boolean palindrome(int reverse, int n) {
 
-public static void palendrome(int reverse,int n){
+        return n == reverse;
+        // if (n == reverse) {
+        //     System.out.println("given number " + n + "is palindrome");
+        // } else {
+        //     System.out.println("given number " + n + " is not palindrome");
+        // }
 
-    if(n == reverse){
-        System.out.println("given number "+ n + "is palendrome");
-    }
-    else{
-        System.out.println("given number "+ n +" is not palendrome");
     }
 
-    
-}
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println(" Check Number Is Palendrome Or Not");
-System.out.println("Enter number");
-int n = sc.nextInt();
+        System.out.println(" Check Number Is palindrome Or Not");
+        System.out.println("Enter number");
+        int n = sc.nextInt();
 
+        int reverse = reverse(n, 0);
+        System.out.println("reverse number is " + reverse);
 
-int reverse = reverse(n, 0);
-System.out.println("reverse number is "+reverse);
-
-
-palendrome(reverse, n);
-
-
-
-
+        palindrome(reverse, n);
 
     }
 }
